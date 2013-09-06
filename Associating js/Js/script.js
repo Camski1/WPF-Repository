49da0e85d33006003991335f5dfa7d8ab904bfe7
Cameron Kozinski <camski20@gmail.com>
2013-09-06 13:31:12 -0400

    Screencast: Order of Operations

--- Associating js/Js/script.js
+++ Associating js/Js/script.js
@@ -41,9 +41,26 @@ var width = 4;
 var height = 5;
 var area = width * height / 2;
 console.log(area);
-*/
+
 var remainder = 32 % 2;
 console.log(remainder);
+*/
+
+var quiz1 = 90;
+var quiz2 = 87;
+var quiz3 = 100;
+var quiz4 = 95;
+
+var average = (quiz1+quiz2+quiz3+quiz4)/4;
+
+//console.log(average)
+
+var length =7;
+var width = 6;
+var perimeter =length*2 + width*2;
+console.log(perimeter);
+
+
 
 
 
