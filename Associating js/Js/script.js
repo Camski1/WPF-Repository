e297b354ccff4820a1a45ef1265e186b9a035b12
Cameron Kozinski <camski20@gmail.com>
2013-09-06 13:15:37 -0400

    Screencast: Arithmetic Operators

--- Associating js/Js/script.js
+++ Associating js/Js/script.js
@@ -8,10 +8,8 @@ console.log (yearBorn);
 
 alert(yearBorn);
 
-
 var age;
 age = 25;
-
 
 var yearOfBirth = 1988;
 
@@ -20,7 +18,6 @@ var isStudent = false;
 var firstName = "Cameron";
 
 var phrace ='I don\'t know \n You will have to speak with the professor ';
-
 
 var familyNames = [ "Cameron", "Kristen", "Brady"];
 
@@ -34,9 +31,15 @@ var a = 2;
 var b;
 b = a + 3;
 console.log(a);
-*/
 
 var yearBorn = 1988;
 var age = 2013 - yearBorn;
 console.log(age);
+*/
+
+var width = 4;
+var height = 5;
+var area = width * height / 2;
+console.log(area);
+
 
