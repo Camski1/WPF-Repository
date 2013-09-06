31e981f49339968e92d454188f97504c3e502820
Cameron Kozinski <camski20@gmail.com>
2013-09-06 13:51:07 -0400

    Screencast: Concatenating Strings

--- Associating js/Js/script.js
+++ Associating js/Js/script.js
@@ -1,68 +1,14 @@
-/*
-var yearBorn = 1988;
+
+ 
+var a = 6;
+var b = 7;
+
+var result = a + b;
+console.log(result);
 
 
-console.log ("Hello World!");
-console.log (yearBorn);
 
 
-alert(yearBorn);
-
-var age;
-age = 25;
-
-var yearOfBirth = 1988;
-
-var isStudent = false;
-
-var firstName = "Cameron";
-
-var phrace ='I don\'t know \n You will have to speak with the professor ';
-
-var familyNames = [ "Cameron", "Kristen", "Brady"];
-
-familyNames[2] = "santa";
-familyNames[3] = "Kermit";
-
-var num = 2
-console.log (familyNames[num]);
-
-var a = 2; 
-var b;
-b = a + 3;
-console.log(a);
-
-var yearBorn = 1988;
-var age = 2013 - yearBorn;
-console.log(age);
-
-
-var width = 4;
-var height = 5;
-var area = width * height / 2;
-console.log(area);
-
-var remainder = 32 % 2;
-console.log(remainder);
-
-
-var quiz1 = 90;
-var quiz2 = 87;
-var quiz3 = 100;
-var quiz4 = 95;
-
-var average = (quiz1+quiz2+quiz3+quiz4)/4;
-
-//console.log(average)
-
-var length =7;
-var width = 6;
-var perimeter =length*2 + width*2;
-console.log(perimeter);
-*/
-var orangeBins = [234, 567, 883];
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log (total);
 
 
 
