9dc15290c77eadc2b18dc7fe4fafc4f1ea4765e6
Cameron Kozinski <camski20@gmail.com>
2013-09-06 15:23:05 -0400

    Problem one converting human years into dog years.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,8 @@
+//Cameron Kozinski 09/06/2013 Expression Worksheet
+//Dog years
+var sparky= 4; 
+var dogYears= 7;
+//multiply sparky's age by the dog years
+var sparkyDogAge= sparky*dogYears;
+//printout for converting human years into dog years
+console.log("Sparky is", sparky, "human years old which is", sparkyDogAge, "in dog years");
