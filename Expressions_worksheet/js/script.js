a02651273542dd598d82baaf64e2b40d1a960321
Cameron Kozinski <camski20@gmail.com>
2013-09-07 13:06:05 -0400

    Average shopping bill

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -18,7 +18,7 @@ var pizzasOrdered= 100;
 var slicesPP= pizzaSlices * pizzasOrdered / peopleParty;
 //Printout number of slices of pizza per person.
 console.log("Each person ate",slicesPP,"slices of pizza at the party.");
-*/
+
 //slices of pie part two
 var pizzaSlices= 8;
 var peopleParty= 35;  //number of people at the party
@@ -28,3 +28,11 @@ var slicesPP= pizzaSlices * pizzasOrdered / peopleParty;
 var sparkyPizza=  totalSlices % peopleParty ; //remander of pizza 
 //the amount of pizza sparky eats print out
 console.log("Sparky got",sparkyPizza,"slices of pizza.");
+*/
+//Average shopping bill
+var billsOver5Weeks= [80.50, 79.25, 50.13, 78.12, 92.75]; //each weeks shoping bill
+var total= billsOver5Weeks[0]+ billsOver5Weeks[1]+ billsOver5Weeks[2]+ billsOver5Weeks[3]+ billsOver5Weeks[4]; //total of the bills over five weeks
+var averageCostPerWeek= total/5; //average of the bills over five weeks
+ //printout of total bills over 5 weeks, and average amount spent per week
+console.log("You have spent a total of $",total," on groceries over 5 weeks. That is an average of $",averageCostPerWeek," per week");
+
