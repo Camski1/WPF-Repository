60d5f28f7afa81619fd7846cbf067c2e9a7ebe52
Cameron Kozinski <camski20@gmail.com>
2013-09-07 12:30:50 -0400

    Slices of pie part two.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -8,7 +8,7 @@ var dogYears= 7;
 var sparkyDogAge= sparky*dogYears;
 //printout for converting human years into dog years
 console.log("Sparky is", sparky, "human years old which is", sparkyDogAge, "in dog years");
-*/
+
 
 //Slice of pie one. 
 var pizzaSlices= 8;
@@ -18,4 +18,13 @@ var pizzasOrdered= 100;
 var slicesPP= pizzaSlices * pizzasOrdered / peopleParty;
 //Printout number of slices of pizza per person.
 console.log("Each person ate",slicesPP,"slices of pizza at the party.");
-
+*/
+//slices of pie part two
+var pizzaSlices= 8;
+var peopleParty= 35;  //number of people at the party
+var pizzasOrdered= 100;
+var totalSlices= pizzaSlices * pizzasOrdered; //total number of slices when *ing pizzaSlices by pizzaOrdered
+var slicesPP= pizzaSlices * pizzasOrdered / peopleParty;
+var sparkyPizza=  totalSlices % peopleParty ; //remander of pizza 
+//the amount of pizza sparky eats print out
+console.log("Sparky got",sparkyPizza,"slices of pizza.");
