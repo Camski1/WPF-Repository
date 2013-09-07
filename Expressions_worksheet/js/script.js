38dd1a13ed589eda3ee56baebe4f15f909e6bed3
Cameron Kozinski <camski20@gmail.com>
2013-09-07 11:40:04 -0400

    Slices of pie one.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,8 +1,21 @@
 //Cameron Kozinski 09/06/2013 Expression Worksheet
 //Dog years
+
+/*
 var sparky= 4; 
 var dogYears= 7;
 //multiply sparky's age by the dog years
 var sparkyDogAge= sparky*dogYears;
 //printout for converting human years into dog years
 console.log("Sparky is", sparky, "human years old which is", sparkyDogAge, "in dog years");
+*/
+
+//Slice of pie one. 
+var pizzaSlices= 8;
+var peopleParty= 35;
+var pizzasOrdered= 100;
+//multiply pizzaSlices by pizzasOrdered and devide by peopleParty
+var slicesPP= pizzaSlices * pizzasOrdered / peopleParty;
+//Printout number of slices of pizza per person.
+console.log("Each person ate",slicesPP,"slices of pizza at the party.");
+
