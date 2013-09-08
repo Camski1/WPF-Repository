d6e940c36bc45139366c512f780fda328bf0e33a
Cameron Kozinski <camski20@gmail.com>
2013-09-07 23:53:56 -0400

    Screencast: Prompt and Alert

--- Associating js/Js/script.js
+++ Associating js/Js/script.js
@@ -5,14 +5,23 @@ var b = 7;
 
 var result = a + b;
 console.log(result);
-*/
+
  
 var a =3;
 a --;
 
 console.log(a);
 
-
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+*/
+var width = prompt("We are calculating the area ov a rectangle. \nPlease enter the width.");
+var height= prompt("Please enter height.");
+var area = width * height;
+//console.log(area);//
+var result = "The area of your rectangle is "
++ area + " sq feet.";
+alert(result);
 
 
 
