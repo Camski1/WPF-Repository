5e27f767e4f0c4329ba5c9486e82207a6183163e
Cameron Kozinski <camski20@gmail.com>
2013-09-08 21:50:43 -0400

    Started working on annual fuel cost calculator. Coded the highway miles per week and highway mpg.

--- Expressions_Personal/Js/script.js
+++ Expressions_Personal/Js/script.js
@@ -2,3 +2,8 @@
 //Cameron Kozinski
 //WPF sec 01
 // 09-08-2013
+
+
+//Annual fuel cost calculator
+var milesPerWeekH/I = prompt("How many miles do you drive per week on the highway/interstate?");
+var avrH/IMpg = prompt("What is your average MPG on the highway/interstate?");
