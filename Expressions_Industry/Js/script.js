8e813e4b95e67a19f119df0de4669b15c9f4695e
Cameron Kozinski <camski20@gmail.com>
2013-09-09 11:20:26 -0400

    Added a var and prompt for how much time is spent writing code.

--- Expressions_Industry/Js/script.js
+++ Expressions_Industry/Js/script.js
@@ -7,3 +7,4 @@
 //Project time estimation calculator 
 alert("The information you give will give you an estimate of how long a project takes you to complete. This will be helpful in estimating the time needed for future projects");
 var linesOfCode = prompt("How many lines of code did you write?");
+var timeSpentOnCode = prompt("How long did you spend writing your code.");
