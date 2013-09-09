26734c8048f70b0e364cc5861d39a395a5d75c23
Cameron Kozinski <camski20@gmail.com>
2013-09-08 21:58:05 -0400

    set up prompts for finding amount of city driving and city mpg.

--- Expressions_Personal/Js/script.js
+++ Expressions_Personal/Js/script.js
@@ -5,5 +5,9 @@
 
 
 //Annual fuel cost calculator
+
 var milesPerWeekH/I = prompt("How many miles do you drive per week on the highway/interstate?");
 var avrH/IMpg = prompt("What is your average MPG on the highway/interstate?");
+var milesPerWeekCity = prompt("How many would you say you drive in the City?");
+var avrCityMpg = prompt("What is your average MPG in the City?");
+
