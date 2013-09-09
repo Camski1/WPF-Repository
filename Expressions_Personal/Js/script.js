8b1a181bbe036c4989d7a5557a75ad11bd93943d
Cameron Kozinski <camski20@gmail.com>
2013-09-08 22:31:02 -0400

    Added an alert telling the user what the information given is for. Also, changed the var names on lines 12 and 13 by simply removing the backslash. Removing the back slash fixed an error I was having. The error was that when I loaded the page nothing was happening.

--- Expressions_Personal/Js/script.js
+++ Expressions_Personal/Js/script.js
@@ -1,3 +1,4 @@
+
 //Expressions_Personal
 //Cameron Kozinski
 //WPF sec 01
@@ -6,8 +7,10 @@
 
 //Annual fuel cost calculator
 
-var milesPerWeekH/I = prompt("How many miles do you drive per week on the highway/interstate?");
-var avrH/IMpg = prompt("What is your average MPG on the highway/interstate?");
+
+prompt("The information you add will calculate your annual fuil cost!");
+var milesPerWeekHI = prompt("How many miles do you drive per week on the highway/interstate?");
+var avrHIMpg = prompt("What is your average MPG on the highway/interstate?");
 var milesPerWeekCity = prompt("How many would you say you drive in the City?");
 var avrCityMpg = prompt("What is your average MPG in the City?");
 
