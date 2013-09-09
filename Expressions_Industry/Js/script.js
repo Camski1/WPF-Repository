37cb66ec5fec768db22802f5e0391f0074167c63
Cameron Kozinski <camski20@gmail.com>
2013-09-09 10:08:22 -0400

    Added alert letting the user know what the information they give is for

--- Expressions_Industry/Js/script.js
+++ Expressions_Industry/Js/script.js
@@ -5,3 +5,4 @@
 
 
 //Project time estimation calculator 
+alert("The information you give will give you an estimate of how long a project takes you to complete. This will be helpful in estimating the time needed for future projects");
