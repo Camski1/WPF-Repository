de2a6025c445ce2a34185ba81eccd7b23b1d1258
Cameron Kozinski <camski20@gmail.com>
2013-09-08 22:39:16 -0400

    Changed the prompt on line 11 to an alert as it didn't need any user input.

--- Expressions_Personal/Js/script.js
+++ Expressions_Personal/Js/script.js
@@ -8,7 +8,7 @@
 //Annual fuel cost calculator
 
 
-prompt("The information you add will calculate your annual fuil cost!");
+alert("The information you add will calculate your annual fuil cost!");
 var milesPerWeekHI = prompt("How many miles do you drive per week on the highway/interstate?");
 var avrHIMpg = prompt("What is your average MPG on the highway/interstate?");
 var milesPerWeekCity = prompt("How many would you say you drive in the City?");
