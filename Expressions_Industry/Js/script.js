252cd71c4544e2505b8d4b2f4134039cac0286a8
Cameron Kozinski <camski20@gmail.com>
2013-09-09 20:55:57 -0400

    added comment to line 12

--- Expressions_Industry/Js/script.js
+++ Expressions_Industry/Js/script.js
@@ -9,7 +9,7 @@
 alert("The information you give will give you an estimate of how long a project takes you to complete. This will be helpful in estimating the time needed for future projects"); //This is an alert letting the user know what the info will be for.
 var linesOfCode = prompt("How many lines of code did you write?"); //This var is for storing the numer of lines of code. the prompt is asking the user for the information. 
 var timeSpentOnCode = prompt("How long did you spend writing your code?"); //This var is for the time spent writting the code. The prompt is asking the user for the information.
-var timeSpentInRes = prompt("How much time did you spend on research?");
+var timeSpentInRes = prompt("How much time did you spend on research?"); //This var is for time spent on research. The prompt is asking the user for the info. 
 var timeSpentOnBreaks = prompt("How much time did you spend taking breaks? I wont tell your boss.");
 
 var timespentArray= [timeSpentOnBreaks, timeSpentInRes, timeSpentOnCode,];
