4cd0a4cc5ebd15dadec04cad4852dc192893b323
Cameron Kozinski <camski20@gmail.com>
2013-09-10 15:37:20 -0400

    Screencast: Relational Expressions

--- Week Three screencasts/script.js
+++ Week Three screencasts/script.js
@@ -1,12 +1,12 @@
 //Cameron Kozinski
 
-var oldEnough = false;
+var kidHeight = 40;
+var minHeight = 48
 
 //If the child is is old enough, print to the console "you can ride!"
-
-if (oldEnough){
+//if the kid is over 48in in height
+if (kidHeight>minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster");
 
 }
-console.log("what comes after");
