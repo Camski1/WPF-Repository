3c9ab1030771cee0a3e64b734eada864eb5eb10e
Cameron Kozinski <camski20@gmail.com>
2013-09-10 15:29:38 -0400

    Screencast: Conditionals Example

---
+++ Week Three screencasts/script.js
@@ -0,0 +1,12 @@
+//Cameron Kozinski
+
+var oldEnough = false;
+
+//If the child is is old enough, print to the console "you can ride!"
+
+if (oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster");
+
+}
+console.log("what comes after");
