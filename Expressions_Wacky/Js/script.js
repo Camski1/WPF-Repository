fc4de488d1867b2a0c4313f9192b21d20aa673ae
Cameron Kozinski <camski20@gmail.com>
2013-09-09 21:45:16 -0400

    Added an example to yearBorn. Also, made a story line var

--- Expressions_Wacky/Js/script.js
+++ Expressions_Wacky/Js/script.js
@@ -6,7 +6,9 @@
 //Really fun story time. 
 alert("The information you give will write an amazing story. Have fun!");
 var userName= prompt("What is your name?");
-var yearBorn= prompt("What is your birth-year?");
+var yearBorn= prompt("What is your birthyear? Example 1988");
 var pizzaTopping= prompt("What is your favorite pizza topping?");
 var favNumber= prompt("What is your favorite number?");
 var favAuthor= prompt("Who is your favorite author?");
+var storyLineNumOne= yearBorn / favNumber;
+
