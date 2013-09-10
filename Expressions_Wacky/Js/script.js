af4aa9c88752be8b5943f9224652e1a8fe7ca2d7
Cameron Kozinski <camski20@gmail.com>
2013-09-09 21:36:22 -0400

    added var/prompt for user's favorite author

--- Expressions_Wacky/Js/script.js
+++ Expressions_Wacky/Js/script.js
@@ -9,4 +9,4 @@ var userName= prompt("What is your name?");
 var yearBorn= prompt("What is your birth-year?");
 var pizzaTopping= prompt("What is your favorite pizza topping?");
 var favNumber= prompt("What is your favorite number?");
-
+var favAuthor= prompt("Who is your favorite author?");
