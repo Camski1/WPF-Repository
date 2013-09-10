6642612a05f1244dc0aeda0151b500d6e159dfeb
Cameron Kozinski <camski20@gmail.com>
2013-09-09 23:07:46 -0400

    finished the story.

--- Expressions_Wacky/Js/script.js
+++ Expressions_Wacky/Js/script.js
@@ -17,8 +17,11 @@ alert("Okay, that is all I need. Get ready for story time.");
 alert("The evil War Lord "+userName+ " was the master of the entire world.");
 alert("Since the year " +yearBorn+" " +userName+ " has destroyed " +storyLineNumTwo+ " towns and cities.");
 alert("But all that was about to change.");
-alert("The brave " +favAuthor+ " was secretly forming a plan to take power for the terrible " +userName);
-alert("Our hero " +favAuthor+ " has found " +userName+ "‘s weakness.");
+alert("The brave " +favAuthor+ " was secretly forming a plan to take power from the terrible " +userName);
+alert("Our hero " +favAuthor+ " has found " +userName+ "s weakness.");
 alert("You see " +favAuthor+ " new that " +userName+ " loved " +pizzaTopping+ " pizza and would eat " +storyLineNumOne+ " of them a week.");
-alert("Late one night as " +userName+ " was playing xbox he could out for his servant to bring him his next " +pizzaTopping);
-
+alert("Late one night as " +userName+ " was playing xbox " +userName+ " called out for his servant to bring the next " +pizzaTopping+ " pizza");
+alert("But " +favAuthor+ " had taken the servants place and slowly walked into the room with the " +pizzaTopping+ " pizza.");
+alert(favAuthor+" walked behind " +userName+ " and hit " +userName+ " on the head with the pizza killing the War Lord and saved the world.");
+alert("The people of the world were so happy that they gave " +favAuthor+ " " +storyLineNumOne*storyLineNumTwo+ " " +pizzaTopping+ " pizzas.");
+alert("END");
