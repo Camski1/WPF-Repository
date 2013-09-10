77d67f851678cfd13c6f57afdab136d1928a5353
Cameron Kozinski <camski20@gmail.com>
2013-09-10 15:40:09 -0400

    Screencast: Condition with Expression

--- Week Three screencasts/script.js
+++ Week Three screencasts/script.js
@@ -1,11 +1,11 @@
 //Cameron Kozinski
 
-var kidHeight = 40;
-var minHeight = 48
-
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
 //If the child is is old enough, print to the console "you can ride!"
 //if the kid is over 48in in height
-if (kidHeight>minHeight){
+if (kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster");
 
