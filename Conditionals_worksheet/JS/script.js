d40233f442c1c3b86d983e3d106003ca6fecf410
Cameron Kozinski <camski20@gmail.com>
2013-09-11 12:00:51 -0400

    Grade Letter Calculator

--- Conditionals_worksheet/JS/script.js
+++ Conditionals_worksheet/JS/script.js
@@ -1,7 +1,8 @@
 //Cameron Kozinski
 // 09-11-2013
-//Celsius to Fahrenheit converter
 
+/*
+//Celsius to Fahrenheit converter
 //This alert will let the user known what the information is for
 alert("Are you having truble converting Fahrenheit or Celsius? Let me help.");
 
@@ -14,4 +15,34 @@ if(fahrOrCels === "C"){
 	alert("The temperature in Fahrenheit is " +celsiusConverter+ " degrees!");
 }else{
 	alert("The temperature in Celsius is " +fahrenheitConverter+ " degrees!");
+}
+*/
+
+//Grade Letter Calculator
+
+alert("The information you give will convert your number grade into a letter grade!");//This alert is letting the user know what the info given is for
+var gradePercent = prompt("What was your grade? Example 100"); //The prompt is asking the user for the grade number, and the var is holding the info given by the user.
+var gradeAPluss = 95; //this var is holding the min grade for a A+
+var gradeA = 90;//this var is holding the min grade for a A
+var gradeBPluss = 85;//this var is holding the min grade for a B+
+var gradeB = 80;//this var is holding the min grade for a B
+var gradeC = 73;//this var is holding the min grade for a C
+var gradeD = 70; //this var is holding the min grade for a D
+//I had no need for a var with the min grade for an F
+
+if(gradePercent >= gradeAPluss){ //if the user grade is greater than or equal to grade A+ aka 95. The alert will print out A+
+	alert("You have a " +gradePercent+ "%, which means you have earned a(n) A+ in the class!")
+ 
+}else if(gradePercent >= gradeA){//if the user grade is greater than or equal to grade A aka 90. The alert will print out A
+	alert("You have a " +gradePercent+ "%, which means you have earned a(n) A in the class!");
+}else if(gradePercent >= gradeBPluss){//if the user grade is greater than or equal to grade B+ aka 85. The alert will print out B+
+	alert("You have a " +gradePercent+ "%, which means you have earned a(n) B+ in the class!");
+}else if(gradePercent >= gradeB){//if the user grade is greater than or equal to grade B aka 80. The alert will print out B
+	alert("You have a " +gradePercent+ "%, which means you have earned a(n) B in the class!");
+}else if(gradePercent >= gradeC){//if the user grade is greater than or equal to grade C aka 73. The alert will print out C
+	alert("You have a " +gradePercent+ "%, which means you have earned a(n) C in the class!");
+}else if (gradePercent >= gradeD){//if the user grade is greater than or equal to grade D aka 70. The alert will print out D
+	alert("You have a " +gradePercent+ "%, which means you have earned a(n) D in the class!");
+}else{//if the user grade is equal to anything else. The alert will print out F
+	alert("You have a " +gradePercent+ "%, which means you have earned a(n) F in the class!");
 }
