3cbf6d983ab6b4d78b4f4994ab8eac01f7b1687f
Cameron Kozinski <camski20@gmail.com>
2013-09-10 20:41:51 -0400

    Screencast: If and else

--- Week Three screencasts/Js/script.js
+++ Week Three screencasts/Js/script.js
@@ -1,11 +1,14 @@
 //Cameron Kozinski
 
-var oldEnough = true;
+var kidHeight = 49;
+var minHeight = 48;
 
 //If the child is is old enough, print to the console "you can ride!"
-
-if (oldEnough){
+//if the kid is over 48in in height
+if(kidHeight > minHeight){
     //code performed if condition is true
-    console.log("You can ride the coaster");
-
+    console.log("You can ride the coaster!");
+}
+else{
+	console.log("Sorry kid, you've got some growing to do first!");
 }
