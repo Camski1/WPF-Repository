164244f1268bfa1f148fc33a1d0ecc3885f8aaf2
Cameron Kozinski <camski20@gmail.com>
2013-09-16 15:09:00 -0400

    started my second question within the first if section.

--- Conditionals_Personal/Js/script.js
+++ Conditionals_Personal/Js/script.js
@@ -6,12 +6,22 @@
 
 alert("This short quiz will help you with simple math skills!");
 
-var userAnswerOne = prompt("Find The Answer. \n 9 * 9 = ?");
+var userAnswerOne = prompt("Find The Answer. \n9 * 9 = ?");
 var trueAnswer = 9 * 9;
 
 if(Number(userAnswerOne) === trueAnswer){
 	alert("Woohoo! Lets try somthing harder.");
 
+		var userAnswerTwoIf = prompt("Find The Answer. \n1/2 + 3/4 = ?");
+		var trueAnswerTwoIf = "1 1/4";
+
+		if(userAnswerTwoIf===trueAnswerTwoIf){
+			alert("Well done!")
+			
+		}else{
+			alert("It's cool lets try again.");
+
+		}
 
 }else{
 	alert("nope");
