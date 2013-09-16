62b30f423be6583db8a929bbd2f4f995430d104a
Cameron Kozinski <camski20@gmail.com>
2013-09-16 15:01:35 -0400

    Made my if or else statements according to the answer given by the user.

--- Conditionals_Personal/Js/script.js
+++ Conditionals_Personal/Js/script.js
@@ -8,3 +8,11 @@ alert("This short quiz will help you with simple math skills!");
 
 var userAnswerOne = prompt("Find The Answer. \n 9 * 9 = ?");
 var trueAnswer = 9 * 9;
+
+if(Number(userAnswerOne) === trueAnswer){
+	alert("Woohoo! Lets try somthing harder.");
+
+
+}else{
+	alert("nope");
+}
