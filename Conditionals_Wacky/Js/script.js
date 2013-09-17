30275650ebe648161d033c3dfa81a06574edce4e
Cameron Kozinski <camski20@gmail.com>
2013-09-16 22:54:15 -0400

    Added the first question and all the vars needed for it. I also added the if, else if, and else.

--- Conditionals_Wacky/Js/script.js
+++ Conditionals_Wacky/Js/script.js
@@ -2,4 +2,17 @@
 //09-16-2013
 //Wacky
 
-//
+//Fun with things in your house.
+
+alert("The information you add will paint a picture of you and you life.");
+var userBooks = prompt("Out of these styles of writting what is you favorite\nFiction, Nonfiction or I dont read?");
+var fiction = "Fiction";
+var nonfiction = "Nonfiction";
+
+if(userBooks === fiction){
+
+}else if(){
+
+}else{
+	
+}
