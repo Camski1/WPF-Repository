6f6b06eaa4babede1eff4fd773b0a9e25a1780e0
Cameron Kozinski <camski20@gmail.com>
2013-09-16 23:17:42 -0400

    Changed the title. Checked some spelling problems. Also, added the if and else according to how the user answers the have you read question.

--- Conditionals_Wacky/Js/script.js
+++ Conditionals_Wacky/Js/script.js
@@ -2,17 +2,23 @@
 //09-16-2013
 //Wacky
 
-//Fun with things in your house.
+//Things get uncomfortable with AI
 
-alert("The information you add will paint a picture of you and you life.");
+alert("The information you add will paint a picture of you and your life.");
 var userBooks = prompt("Out of these styles of writting what is you favorite\nFiction, Nonfiction or I dont read?");
 var fiction = "Fiction";
 var nonfiction = "Nonfiction";
 
 if(userBooks === fiction){
 	alert("Very cool I LOVE Fiction!");
-		var haveYouReadGalaxy = prompt("The Hitchhiker's Guide to the Galaxy?\nYes or No");
-		
+		var haveYouReadGalaxy = prompt("Have you read, The Hitchhiker's Guide to the Galaxy?\nYes or No");
+		if(haveYouReadGalaxy === "Yes"){
+			alert("That is so cool you seem super awesome.");
+
+		}else{
+			alert("It is really a great book. If you would like I can lend it to you.");
+
+		}
 
 }else if(userBooks === nonfiction){
 
