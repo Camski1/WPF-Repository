4596930f151cfb21f5d2cb781b341a98474bbe3f
Cameron Kozinski <camski20@gmail.com>
2013-09-17 22:24:57 -0400

    starting over

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -2,20 +2,3 @@
 //09-17-2013
 //Industry
 
-//How much time do you have to finish a project?
-
-alert("This will help you find out how many hour you have left to finish your project!");
-
-var timeLine = prompt("How many days do you have until your project is needed?");
-var hrsPerDay = prompt("How many hours per day do you work?");
-var daysPerWeek = prompt("How many days per week do you work?");
-var extraHrsYesNo = prompt("Are you willing to add extra hours each day to get the job finished more quickly? Yes or No.")
-var hrsNeededForProj = Number(hrsPerDay) * Number(timeLine);
-if(extraHrsYesNo === "Yes"){
-	var extraHrs = prompt("How many hours per day will you add?");
-	var totalHrsPerDay = Number(extraHrs) + Number(hrsPerDay);
-	var timeLeft = hrsNeededForProj / 
-	
-}else{
-
-}
