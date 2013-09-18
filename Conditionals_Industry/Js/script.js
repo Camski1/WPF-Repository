332a9b009ea449fb5884a9ae5f2ae788288df874
Cameron Kozinski <camski20@gmail.com>
2013-09-18 09:52:55 -0400

    added two vars for a great lunch = 20 and a good lunch = 10.

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -9,4 +9,6 @@ alert("This page will help you find what you can spend on your employee's for lu
 var budgetTot = prompt("What is your budget for lunch?");
 var empoyeeNum = prompt("How many employees will be going with you to lunch?");
 var ppaBudget = budgetTot / (Number(empoyeeNum) + 1); 
+var greatLunch = 20;
+var goodLunch = 10;
 
