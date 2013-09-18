de9a43158931dfc41117e1fbda4fc40a6a6fd4fd
Cameron Kozinski <camski20@gmail.com>
2013-09-17 22:05:13 -0400

    moved the var for finding the number of hrs left in the project above the if statement so that I can use it in both the if and else.

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -10,11 +10,11 @@ var timeLine = prompt("How many days do you have to complete your project?");
 var hrsPerDay = prompt("How many hours per day do you work?");
 var daysPerWeek = prompt("How many days per week do you work?");
 var extraHrsYesNo = prompt("Are you willing to add extra hours each day to get the job finished more quickly? Yes or No.")
-
+var hrsNeededForProj = Number(hrsPerDay) * Number(timeLine);
 if(extraHrsYesNo === "Yes"){
 	var extraHrs = prompt("How many hours per day will you add?");
 	var totalHrsPerDay = Number(extraHrs) + Number(hrsPerDay);
-	var hrsNeededForProj = Number(hrsPerDay) * Number(timeLine);
+	
 	 
 
 }else{
