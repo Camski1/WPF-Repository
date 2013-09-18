1d3de9ad7435c1f5efa85dc39a98218357530b84
Cameron Kozinski <camski20@gmail.com>
2013-09-17 22:10:52 -0400

    changed wording of the first prompt

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -6,7 +6,7 @@
 
 alert("This will help you find out how many hour you have left to finish your project!");
 
-var timeLine = prompt("How many days do you have to complete your project?");
+var timeLine = prompt("How many days do you have until your project is needed?");
 var hrsPerDay = prompt("How many hours per day do you work?");
 var daysPerWeek = prompt("How many days per week do you work?");
 var extraHrsYesNo = prompt("Are you willing to add extra hours each day to get the job finished more quickly? Yes or No.")
@@ -14,9 +14,8 @@ var hrsNeededForProj = Number(hrsPerDay) * Number(timeLine);
 if(extraHrsYesNo === "Yes"){
 	var extraHrs = prompt("How many hours per day will you add?");
 	var totalHrsPerDay = Number(extraHrs) + Number(hrsPerDay);
+	var timeLeft = hrsNeededForProj / 
 	
-	 
-
 }else{
 
 }
