732bf09394e981e6d03f29ddc0bf5e769646b413
Cameron Kozinski <camski20@gmail.com>
2013-09-17 21:20:04 -0400

    Added a question for if the user will be willing to work extra hour. also added my if else sections.

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -9,4 +9,11 @@ alert("This will help you find out how many workers you will need to finish a jo
 var timeLine = prompt("How many days do you have to complete your project?");
 var hrsPerDay = prompt("How many hours per day do your employees work?");
 var daysPerWeek = prompt("How many days per week do your employees work?");
+var extraHrsYesNo = prompt("Are you willing to add extra hours each day to get the job finished more quickly")
 
+if(extraHrs === "Yes"){
+	var extraHrs = prompt("How many hours per day will you add?");
+	
+}else{
+
+}
