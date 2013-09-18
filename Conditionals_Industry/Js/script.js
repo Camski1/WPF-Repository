7d21edb9650ebb91daa6bb6de9b2609196458e5b
Cameron Kozinski <camski20@gmail.com>
2013-09-17 21:05:12 -0400

    changed my mind on what i will be finding. also added an alert telling the user what the info will be for, and added the first prompt.

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -2,5 +2,9 @@
 //09-17-2013
 //Industry
 
-//helping you convert px to em
+//How many people will you need?
+
+alert("This will help you find out how many workers you will need to finish a job!");
+
+var time = prompt("How many days do you have to complete your project?");
 
