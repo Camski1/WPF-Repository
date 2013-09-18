bf877839bc3d9ae77296fc3f6700344c07a00e4c
Cameron Kozinski <camski20@gmail.com>
2013-09-18 10:47:26 -0400

    Added more comments to the good lunch great lunch vars.

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -9,8 +9,8 @@ alert("This page will help you find what you can spend on your employee's for lu
 var budgetTot = prompt("What is your budget for lunch?");//That var is going to hold the budget total, and the prompt is getting the info from the user.
 var empoyeeNum = prompt("How many employees will be going with you to lunch?");//The var is holding the number of employees going to lunch, and the prompt is getting the info from the user.
 var ppaBudget = budgetTot / (Number(empoyeeNum) + 1); //PPA stands for per person average and that is the var holding that info 
-var greatLunch = prompt("Hom much money do you have to spend per person if are going to have a great lunch?");
-var goodLunch = prompt("Hom much money do you have to spend per person if are going to have a good lunch?");
+var greatLunch = prompt("Hom much money do you have to spend per person if are going to have a great lunch?"); //The var is holding what the user thinks will = a great lunch and the prompt is getting the info from the user.
+var goodLunch = prompt("Hom much money do you have to spend per person if are going to have a good lunch?"); //The var is holding what the user thinks will = a good lunch and the prompt is getting the info from the user.
 
 if(ppaBudget >= Number(greatLunch)){
 	alert("You have plenty to spend per person! You can spend $" +ppaBudget+ " per person.");
