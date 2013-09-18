0acd2263cff9578fc398fff1f56c410e3046977b
Cameron Kozinski <camski20@gmail.com>
2013-09-17 22:03:20 -0400

    Changed the idea again. also added more vars to the if section finding the new number of hrs per day and finding the hrs needed for the project.

--- Conditionals_Industry/Js/script.js
+++ Conditionals_Industry/Js/script.js
@@ -2,17 +2,20 @@
 //09-17-2013
 //Industry
 
-//How many people will you need?
+//How much time do you have to finish a project?
 
-alert("This will help you find out how many workers you will need to finish a job!");
+alert("This will help you find out how many hour you have left to finish your project!");
 
 var timeLine = prompt("How many days do you have to complete your project?");
-var hrsPerDay = prompt("How many hours per day do your employees work?");
-var daysPerWeek = prompt("How many days per week do your employees work?");
+var hrsPerDay = prompt("How many hours per day do you work?");
+var daysPerWeek = prompt("How many days per week do you work?");
 var extraHrsYesNo = prompt("Are you willing to add extra hours each day to get the job finished more quickly? Yes or No.")
 
 if(extraHrsYesNo === "Yes"){
 	var extraHrs = prompt("How many hours per day will you add?");
+	var totalHrsPerDay = Number(extraHrs) + Number(hrsPerDay);
+	var hrsNeededForProj = Number(hrsPerDay) * Number(timeLine);
+	 
 
 }else{
 
