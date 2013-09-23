9d3e13918076a96929ab0879a702d78dc7ba9fa4
Cameron Kozinski <camski20@gmail.com>
2013-09-23 15:32:46 -0400

    second worksheet problem.

--- Functions_worksheet/Js/script.js
+++ Functions_worksheet/Js/script.js
@@ -3,6 +3,7 @@
 //09-23-13
 
 //Circumference
+/*
 
 var circumference = function(radius){
 	var circOfACircle = radius * 3.14;
@@ -10,5 +11,16 @@ var circumference = function(radius){
 }
 var circOfACircle = circumference(3.5);
 console.log("The circumference of the circle is " +circOfACircle+ " inches.");
+*/
+
+//Stung!
+var cameron = 145;
+
+var beeSting = function(lb){
+	var numOfStings = lb * 8.666666667;
+	return numOfStings;
+}
+var stingsToKill = beeSting(cameron);
+console.log("It takes " +stingsToKill+ " bee stings to kill a " +cameron+ " pound animal!")
 
 
