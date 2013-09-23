0f1b2210b60b4ee7c2fb0b89c3488ea3b559e0bf
Cameron Kozinski <camski20@gmail.com>
2013-09-22 22:00:04 -0400

    js. week 4 screencasts.

---
+++ Week_4_Screencast/Js/script.js
@@ -0,0 +1,27 @@
+//Cameron Kozinski
+
+//while Loop
+/*
+var b = 10;
+while(b > 0 ){
+	console.log(b + " kegs on the wall.");
+	b--;
+}
+
+
+//do while
+
+var c = 10;
+do{
+	console.log(c + " kegs on the wall.")
+	c--;
+}while(c > 0);
+
+
+//for loop
+
+for(var i = 10; i > 0; i--){
+	console.log(i + " kegs on the wall.");
+}
+*/
+
