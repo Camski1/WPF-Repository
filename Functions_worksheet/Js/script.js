910a13f7c20e0e3054ae1d52e53fa29c2abda6dc
Cameron Kozinski <camski20@gmail.com>
2013-09-23 15:16:56 -0400

    first worksheet problem.

---
+++ Functions_worksheet/Js/script.js
@@ -0,0 +1,14 @@
+//Cameron Kozinski
+//Functions_worksheet
+//09-23-13
+
+//Circumference
+
+var circumference = function(radius){
+	var circOfACircle = radius * 3.14;
+	return circOfACircle;
+}
+var circOfACircle = circumference(3.5);
+console.log("The circumference of the circle is " +circOfACircle+ " inches.");
+
+
