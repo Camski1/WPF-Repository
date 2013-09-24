c2876f8eacb2c66e71ae8bdaf120d82a7293aaa6
Cameron Kozinski <camski20@gmail.com>
2013-09-23 23:32:50 -0400

    changed the if statement  to a ternary and alerted the user of the info

--- Functions_Personal/Js/script.js
+++ Functions_Personal/Js/script.js
@@ -31,8 +31,6 @@ function costForPaint(cpg, pyn){
 
 var paintTotal = costForPaint(costPerGal,paintYouNeed);
 
-if(){
+var total = (paintTotal <= paintBugdet) ? "You can get the pait the total was only $" +paintTotal+"" : "Sorry the paint is more then your budget of $" +paintBugdet+ " it is $" +paintTotal+"";
 
-}else{
-
-}
+alert(total);
