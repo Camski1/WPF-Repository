122a12c070d6de97785d5f40078238dee9adfeec
Cameron Kozinski <camski20@gmail.com>
2013-09-23 21:58:14 -0400

    Added two more vars and prompt asking the user for more info.

--- Functions_Personal/Js/script.js
+++ Functions_Personal/Js/script.js
@@ -8,3 +8,6 @@ alert("This will help you find the amount of paint you will need.");
 var wallHeight = prompt("What is the height of the wall you be painting?");
 var wallWidth = prompt("What is the width of the wall you will be painting?");
 var numCoats = prompt("How many coats of paint will you need?");
+var howMuchPDoYouGot = prompt("How many gallons of paint do you have?");
+var sqrFtPaintCover = prompt("How many square feet will a gallon of paint cover?");
+
