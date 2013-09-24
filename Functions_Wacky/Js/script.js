f985543a5827c9abea450f7748517f57bdcd061f
Cameron Kozinski <camski20@gmail.com>
2013-09-24 09:41:16 -0400

    Added the vars and prompt asking the user for info

--- Functions_Wacky/Js/script.js
+++ Functions_Wacky/Js/script.js
@@ -6,3 +6,6 @@
 
 alert("This page will add a new spin to added holiday pounds.");
 
+var preHolidayLb = prompt("What did you weigh before the holidays?");
+var curWeight = prompt("And what the you weigh now?");
+var maleOrFe = prompt("Are you a Boy or Girl?");
