ab8012f9a6e46af5996f9fcb49b2b78d684ff552
Cameron Kozinski <camski20@gmail.com>
2013-09-24 11:05:27 -0400

    Added vars for sex of user and a var if the user is one year old. Also used the function to set a var for male users.

--- Functions_Wacky/Js/script.js
+++ Functions_Wacky/Js/script.js
@@ -10,8 +10,13 @@ var preHolidayLb = prompt("What did you weigh before the holidays?");
 var curWeight = prompt("And what the you weigh now?");
 var maleOrFe = prompt("Are you a Boy or Girl?");
 var userAge = prompt("How old are you?");
+var malePercent = .65;
+var fePercent = .55;
+var underOne = .75;
 
-var userWaterWeight = function(){
+var userWaterWeight = function(oldW, newW, percent){
 	var waterWeight = oldW + newW - oldW / percent;
 	return waterWeight;
 }
+
+var maleWaterW = userWaterWeight(preHolidayLb,curWeight,malePercent);
