3d344b082af1a7018799a68d761aca91c9086371
Cameron Kozinski <camski20@gmail.com>
2013-09-23 21:52:36 -0400

    setup the alert telling the user what the info is for. Also, made three vars and prompts.

--- Functions_Personal/Js/script.js
+++ Functions_Personal/Js/script.js
@@ -4,3 +4,7 @@
 
 //Paint Party 
 
+alert("This will help you find the amount of paint you will need.");
+var wallHeight = prompt("What is the height of the wall you be painting?");
+var wallWidth = prompt("What is the width of the wall you will be painting?");
+var numCoats = prompt("How many coats of paint will you need?");
