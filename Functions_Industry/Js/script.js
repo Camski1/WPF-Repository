89602447a8165a1eb60e552b40d236bfc9ff877f
Cameron Kozinski <camski20@gmail.com>
2013-09-24 22:48:26 -0400

    finished the function.

--- Functions_Industry/Js/script.js
+++ Functions_Industry/Js/script.js
@@ -11,7 +11,11 @@ var numOfYears = prompt("How many years have you been with the company?");
 var roleOverDays = prompt("How many vacation days do you have left from last year?");
 var yearOne = 7;
 var extraYears = numOfYears - 1;
+var yearsAfter = 3;
 
-var vacationDays = function(){
+var vacationDays = function(extraYears, yearsAfter, roleOverDays, yearOne){
+	var numOfDays = extraYears * yearsAfter + roleOverDays + yearOne;
 
 }
+
+
