27dc4600253c69c8ea5a2ebad9b35d3567ddd562
Cameron Kozinski <camski20@gmail.com>
2013-09-24 22:14:04 -0400

    Added an additional alert.

--- Functions_Industry/Js/script.js
+++ Functions_Industry/Js/script.js
@@ -5,5 +5,6 @@
 //Vacation Days
 
 alert("This page will help you find the number of Vacation Days you have this year. On your first year you get 7 day. After that you get 3 extra days for each year of work!");
+alert("If you don't use the vacation time you can role it over to the next year, but if the time is not used the following year that time will be lost. ")
 
 var numOfYears = prompt("How many years have you been with the company?");
