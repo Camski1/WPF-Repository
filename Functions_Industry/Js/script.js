15ffb35a53c41fa8ecf20064315528b29d0c7f15
Cameron Kozinski <camski20@gmail.com>
2013-09-25 21:52:10 -0400

    Added more comments

--- Functions_Industry/Js/script.js
+++ Functions_Industry/Js/script.js
@@ -7,7 +7,7 @@
 alert("This page will help you find the number of Vacation Days you have this year. On your first year you get 7 day. After that you get 3 extra days for each year of work!");
 alert("If you don't use the vacation time you can role it over to the next year, but if the time is not used the following year that time will be lost. ")
 
-var numOfYears = prompt("How many years have you been with the company?");
+var numOfYears = prompt("How many years have you been with the company?");//This var is holding the number of years the user has been with the company
 var roleOverDays = prompt("How many vacation days do you have left from last year?");
 var yearOne = 7;
 var extraYears = numOfYears - 1;
